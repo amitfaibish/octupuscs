dc17c60296e9f0f40869bec38f71e19015dd01e2
root <root@amit1.myguest.virtualbox.org>
2023-05-27 18:48:48 +0300

    final_new

--- app/app.js
+++ app/app.js
@@ -3,11 +3,10 @@ const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = 3000;
-const mongoURL = 'mongodb://db:27017'; // Update with your MongoDB connection URL
+const mongoURL = 'mongodb://db:27017'; 
 
-// Define the route for the homepage
 app.get('/', (req, res) => {
-  // Connect to MongoDB
+  
   MongoClient.connect(mongoURL, (err, client) => {
     if (err) {
       console.error('Failed to connect to MongoDB:', err);
@@ -15,28 +14,33 @@ app.get('/', (req, res) => {
       return;
     }
 
-    // Access the database and collection
-    const db = client.db('mydb'); // Replace 'your_database' with your actual database name
-    const collection = db.collection('fruits'); // Replace 'your_collection' with your actual collection name
+    const db = client.db('mydb'); 
+    const collection = db.collection('fruits'); 
 
-    // Query the database
-    collection.findOne({ name: 'apples' }, { projection: { _id: 0, qty: 1 } }, (err, doc) => {
-        if (err) {
+    collection.aggregate([
+      {
+        $match: { name: 'apples' }
+      },
+      {
+        $group: {
+          _id: null,
+          totalQty: { $sum: '$qty' }
+        }
+      }
+    ]).toArray((err, result) => {
+      if (err) {
         console.error('Failed to retrieve data from MongoDB:', err);
         res.status(500).send('Failed to retrieve data from MongoDB');
       } else {
-        const qty = doc.qty;
-        // Render the quantity along with "hello world!"
-        res.send(`Hello, world! Quantity of apples: ${qty}`);
+        const totalQty = result.length > 0 ? result[0].totalQty : 0;
+        res.send(`Hello world! Total quantity of apples from all the document's at the DB: ${totalQty}`);
       }
 
-      // Close the MongoDB connection
       client.close();
     });
   });
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
